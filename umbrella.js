86d56d85044dc681093af5d73eb70a40444b4e9b
Phillip-M-Tham <phillip.tham@gmail.com>
2026-03-28 21:51:20 -0400

    adding glowing animation to base lighting

--- umbrella.js
+++ umbrella.js
@@ -6,7 +6,7 @@ canvas.height = window.innerHeight;
 
 let t=0;
 
-function drawGlow() {
+function drawBaseLighting() {
     const pulse = (Math.sin(t) +1)/2; // 0 to 1
 
     //backgorund
@@ -14,32 +14,40 @@ function drawGlow() {
     ctx.fillRect(0, 0, canvas.width, canvas.height);
 
     //global red wash
-    ctx.fillStyle = "rgba(255,0,0,0.8)";
+    ctx.fillStyle = "rgba(255,0,0,0.25)";
     ctx.fillRect(0, 0, canvas.width, canvas.height);
-    //const radius = 200 + pulse * 100; // 200 to 300
 
     //dynamic center
     const cx = canvas.width/2 + Math.sin(t*0.5)*100; // oscillate horizontally
     const cy = canvas.height/2 + Math.cos(t*0.3)*60; // oscillate vertically
 
     const maxRadius = Math.max(canvas.width, canvas.height);
-    const radius = maxRadius * (.6 + pulse+.4);
+    const radius = maxRadius * (0.6 + pulse * 0.4);
 
     const gradient = ctx.createRadialGradient(cx, cy, 0, cx, cy, radius);
 
-    gradient.addColorStop(0, `rgba(255,0,0,${.6 + pulse * .4})`);
-    gradient.addColorStop(1, 'rgba(255,0,0,0.1)');
+    gradient.addColorStop(0, `rgba(255,0,0,${0.8 + pulse * 0.2})`);
+    gradient.addColorStop(0.4, 'rgba(255,0,0,0.5)');
+    gradient.addColorStop(1, 'rgba(255,0,0,0.25)');
 
     ctx.fillStyle = gradient;
     ctx.fillRect(0, 0, canvas.width, canvas.height);
 
 }
 
-drawGlow();
+function glow(){
+    t+= 0.03;
+    drawBaseLighting();
+    requestAnimationFrame(glow);
+}
 
+glow();
+
+
+//update dynamically when the window is resized
 window.addEventListener('resize', () => {
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
 
-    drawGlow();
+    glow();
 });
